e89804aba7a47b8ce454136b0bb83720f3bec36c
Suhaas Narayan <suhaas2002@gmail.com>
2025-03-11 16:04:21 -0400

    Removed LanguageID string code

--- undertale/datasets/transforms/segment/ghidra.py
+++ undertale/datasets/transforms/segment/ghidra.py
@@ -34,24 +34,6 @@ class GhidraFunctionSegment(Map):
 
             with open(binary, "wb") as f:
                 f.write(sample)
-            
-            architecture = sample.get("architecture")
-
-            if not architecture:
-                raise GhidraSegmentError(
-                    "dataset does not specify an architecture — please provide one"
-                )
-
-            if architecture == "x64":
-                language = "x86:LE:64:default"
-            elif architecture == "x86":
-                language = "x86:LE:32:default"
-            elif architecture == "arm64":
-                language = "AARCH64:LE:64:v8A"
-            else:
-                raise GhidraSegmentError(
-                    f"invalid architecture '{architecture}' for sample at row {index}; options are x64, x86, arm64"
-                )
 
             # Sets Java max heap size to available system RAM
             if not pyhidra.launcher.jpype.isJVMStarted():
@@ -61,49 +43,55 @@ class GhidraFunctionSegment(Map):
                 launcher = pyhidra.launcher.PyhidraLauncher()
                 launcher.add_vmargs(*jvm_args)
 
-            with pyhidra.open_program(binary, language=language) as api:
-                program = api.getCurrentProgram()
-                listing = program.getListing()
+            try:
+                with pyhidra.open_program(binary) as api:
+                    program = api.getCurrentProgram()
+                    listing = program.getListing()
 
-                for function in listing.getFunctions(True):
-                    # Skip non-local functions.
-                    if function.isExternal() or function.isThunk():
-                        continue
+                    for function in listing.getFunctions(True):
+                        # Skip non-local functions.
+                        if function.isExternal() or function.isThunk():
+                            continue
 
-                    base = program.getAddressMap().getImageBase().getOffset()
-                    body = function.getBody()
-                    start = body.getMinAddress().getOffset()
-                    end = body.getMaxAddress().getOffset()
+                        base = program.getAddressMap().getImageBase().getOffset()
+                        body = function.getBody()
+                        start = body.getMinAddress().getOffset()
+                        end = body.getMaxAddress().getOffset()
 
-                    function = {
-                        "code": sample[start - base : end - base],
-                        "function_name": function.getName(),
-                    }
+                        function = {
+                            "code": sample[start - base : end - base],
+                            "function_name": function.getName(),
+                        }
 
-                    # Duplicate remaining fields from `batch`.
-                    #
-                    # This allows us to propagate any fields defined on the
-                    # whole binary to each function in that binary.
-                    for key in batch:
-                        function[key] = batch[key][idx]
+                        # Duplicate remaining fields from `batch`.
+                        #
+                        # This allows us to propagate any fields defined on the
+                        # whole binary to each function in that binary.
+                        for key in batch:
+                            function[key] = batch[key][idx]
 
-                    sample_functions.append(function)
-            if len(sample_functions) > 0: 
-                batch_functions.extend(sample_functions)
-            else:
-                logger.warning(
-                    f"Ghidra couldn't find local functions for sample at row {row_idx} - removed from dataset"
+                        sample_functions.append(function)
+                if len(sample_functions) > 0: 
+                    batch_functions.extend(sample_functions)
+                else:
+                    logger.warning(
+                        f"Ghidra couldn't find local functions for sample at row {row_idx} - removed from dataset"
+                    )
+
+            except Exception as e:
+                logger.error(
+                    f"failed to process sample at row {index} with error: {e} - removed from dataset"
                 )
 
-        # Convert to the format that the Datasets library expects.
-        #
-        # They want a dictionary of lists, rather than a list of dictionaries.
-        if len(batch_functions) == 0:
-            delete_batch = {}
-            for key in batch:
-                delete_batch[key] = []
-            delete_batch["code"] = delete_batch["function_name"] = []
-            return delete_batch
+                # Convert to the format that the Datasets library expects.
+                #
+                # They want a dictionary of lists, rather than a list of dictionaries.
+                if len(batch_functions) == 0:
+                    delete_batch = {}
+                    for key in batch:
+                        delete_batch[key] = []
+                    delete_batch["code"] = delete_batch["function_name"] = []
+                    return delete_batch
 
         batch_functions = {
             k: [f[k] for f in batch_functions] for k in batch_functions[0]
