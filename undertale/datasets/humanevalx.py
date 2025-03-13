c9450d05b3a3c098cfc9fe8a1c5732b13d40f9ca
Alexander Interrante-Grant <a.interrantegrant@gmail.com>
2025-03-13 16:32:23 -0400

    cleanup

--- undertale/datasets/humanevalx.py
+++ undertale/datasets/humanevalx.py
@@ -1,7 +1,8 @@
+import argparse
 import logging
 
 from datatrove.data import Document, DocumentsPipeline
-from datatrove.executor import LocalPipelineExecutor
+from datatrove.executor import LocalPipelineExecutor, SlurmPipelineExecutor
 from datatrove.pipeline.base import PipelineStep
 from datatrove.pipeline.readers import HuggingFaceDatasetReader
 from datatrove.pipeline.writers import ParquetWriter
@@ -17,20 +18,6 @@ def adapt_humanevalx_from_huggingface(
         "text": f"{data['declaration']}{data['canonical_solution']}",
         "metadata": {"summary": data["prompt"]},
     }
-
-
-def adapt_humanevalx_to_parquet(self, document: Document) -> dict:
-    sample = {}
-
-    sample["id"] = document.id
-    sample["code"] = document.text
-
-    for k, v in document.metadata.items():
-        if k in ["dataset"]:
-            continue
-        sample[k] = v
-
-    return sample
 
 
 class CompileCpp(PipelineStep):
@@ -102,12 +89,88 @@ humanevalx = [
         adapter=adapt_humanevalx_from_huggingface,
     ),
     CompileCpp(),
-    ParquetWriter("output", adapter=adapt_humanevalx_to_parquet),
 ]
 
 
+def adapt_to_flatten(self, document: Document) -> dict:
+    sample = {}
+
+    sample["id"] = document.id
+    sample["code"] = document.text
+
+    for k, v in document.metadata.items():
+        sample[k] = v
+
+    return sample
+
+
+writers = {
+    "parquet": lambda output: ParquetWriter(output, adapter=adapt_to_flatten),
+}
+
+
+executors = {
+    "local": lambda pipeline, options: LocalPipelineExecutor(pipeline),
+    "slurm": lambda pipeline, options: SlurmPipelineExecutor(
+        pipeline,
+        tasks=options.get("slurm_tasks"),
+        time=options.get("slurm_time"),
+        job_name=options.get("slurm_job_name"),
+        mem_per_cpu_gb=options.get("slurm_mem_per_cpu"),
+        cpus_per_task=options.get("slurm_cpus_per_task"),
+        max_array_launch_parallel=True,
+        partition=options.get("slurm_partition"),
+    ),
+}
+
+
 if __name__ == "__main__":
-    executor = LocalPipelineExecutor(
-        pipeline=humanevalx,
+    parser = argparse.ArgumentParser(description="process the HumanEval-X dataset")
+
+    parser.add_argument(
+        "-e",
+        "--executor",
+        choices=executors,
+        default="local",
+        help="executor on which to run the given pipeline",
     )
+
+    parser.add_argument(
+        "--slurm-tasks", type=int, default=32, help="number of parallel tasks to run"
+    )
+    parser.add_argument(
+        "--slurm-time",
+        default="02:00:00",
+        help="maximum time a job can be allowed to run",
+    )
+    parser.add_argument(
+        "--slurm-job-name", default="build-dataset", help="a name for this job"
+    )
+    parser.add_argument(
+        "--slurm-mem-per-cpu", type=int, default=1, help="maximum memory per cpu (GB)"
+    )
+    parser.add_argument(
+        "--slurm-cpus-per-task", type=int, default=4, help="CPUs allocated to each task"
+    )
+    parser.add_argument(
+        "--slurm-partition", help="partition to which this job should be submitted"
+    )
+
+    parser.add_argument(
+        "-w",
+        "--writer",
+        choices=writers,
+        default="parquet",
+        help="output writer (format)",
+    )
+
+    parser.add_argument("output", help="output location")
+
+    arguments = parser.parse_args()
+
+    pipeline = humanevalx.copy()
+    pipeline.append(writers[arguments.writer](arguments.output))
+
+    executor = executors[arguments.executor](pipeline, arguments.__dict__)
+
     executor.run()
