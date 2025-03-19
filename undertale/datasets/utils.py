06e56e337373da98ee9871e574156d9f5358f0f4
Alexander Interrante-Grant <a.interrantegrant@gmail.com>
2025-03-19 13:49:59 -0400

    move utils to `datasets` module call

--- undertale/datasets/utils.py
+++ undertale/datasets/utils.py
@@ -1,15 +1,11 @@
 import argparse
-import code
 import logging
 from typing import Callable, Dict, List
 
-import datasets
 from datatrove.data import Document
 from datatrove.executor import LocalPipelineExecutor, SlurmPipelineExecutor
 from datatrove.pipeline.base import PipelineStep
 from datatrove.pipeline.writers import ParquetWriter
-
-from .. import logging as undertale_logging
 
 logger = logging.getLogger(__name__)
 
@@ -132,35 +128,3 @@ def main(
     executor = executors[arguments.executor](pipeline, arguments.__dict__)
 
     executor.run()
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="utilities for managing datasets")
-
-    subparsers = parser.add_subparsers(
-        dest="command", required=True, help="dataset utility to run"
-    )
-
-    shell_parser = subparsers.add_parser(
-        "shell",
-        help="load a dataset and open a python shell for exploration",
-    )
-
-    shell_parser.add_argument(
-        "path",
-        help="path to a dataset file to load (or the name of one on the HuggingFace hub)",
-    )
-
-    arguments = parser.parse_args()
-
-    undertale_logging.setup_logging()
-
-    if arguments.command == "shell":
-        try:
-            dataset = datasets.load_dataset(arguments.path)
-        except Exception as e:
-            logger.critical(e)
-            exit(1)
-
-        logger.info("the loaded dataset is available in the `dataset` variable")
-        code.interact(local={"dataset": dataset})
