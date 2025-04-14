1f0a194227ae31b6d216e7279f6024ace038e566
Alexander Interrante-Grant <a.interrantegrant@gmail.com>
2025-04-14 16:27:17 -0400

    reorganization of assemblage datasets

--- undertale/datasets/assemblage/vcpkg.py
+++ undertale/datasets/assemblage/vcpkg.py
@@ -1,40 +1,20 @@
-"""
-This is a big dataset. In order to create it, you will have to arrange
-for enough disk space.  Also, note that the code for parsing was
-developed and run on a machine with 512GB RAM.  Takes about 1.5 hours
-to parse on that machine, FYI.
-
-Disk space requirements.
-
-1. About 20GB for the two zip files that come from the URL above:
-   winpe_licensed.zip and winpe_licensed.sqlite.zip. These should be
-   in the directory pointed to by `path` argument.
-
-2. These uncompress into about 102 GB in same directory.
-
-3. Hugging face needs to do mysterious big things in your ~/.cache
-   directory. If that's very big (I have no idea how much space it
-   needs) then fine.  Otherwise, set the HF_HOME env variable to
-   somewhere very large and HF will use that space instead of ~/.cache.
-
-4. The default output dir (unless you set the
-   UNDERTALE_DATASETS_DIRECTORY environment variable) is
-   ~/undertale_shared.  The dataset generated there, in the form of
-   arrow files, will be about 102GB.  So make sure there is enough
-   space there.
-"""
-
 import logging
 import os
-import sqlite3 as sqlite
+import sqlite3
 import time
 
-import pefile
-from datatrove.data import Document
-from datatrove.pipeline.readers.base import BaseReader
+# import typing
+from pathlib import Path
 
-from .base import Dataset, main
-from .pipeline.disassemblers import RadareDisassembler
+import pefile
+from datatrove.data import Document, DocumentsPipeline
+from datatrove.executor import SlurmPipelineExecutor
+from datatrove.pipeline.readers import ParquetReader
+from datatrove.pipeline.readers.base import BaseReader
+from datatrove.pipeline.writers import ParquetWriter
+
+from ..base import DEFAULT_DATASETS_DIRECTORY, Dataset, main
+from ..pipeline.disassemblers import RadareDisassembler
 
 logger = logging.getLogger(__name__)
 
@@ -50,49 +30,46 @@ def tick():
     delta = t - last_time
     f_delta = t - first_time
     last_time = t
-    logger.info(f"tick={delta} el={f_delta}")
+    return (delta, f_delta)
 
 
-class AssemblageWindowsReader(BaseReader):
+def create_directory(*args, **kwargs):
+    import os
+    import shutil
+
+    USER = os.environ.get("USER")
+    dst = f"/state/partition1/user/{USER}"
+    os.makedirs(dst, exist_ok=True)
+    shutil.copytree(
+        f"/home/gridsan/{USER}/undertale", f"{dst}/undertale", dirs_exist_ok=True
+    )
+    os.chdir(f"{dst}/undertale")
+
+
+class AssemblageVcpkgReader(BaseReader):
     type = "📖 - READER"
-    name = "A - Assemblage"
+    name = "A - AssemblageVcpkg"
 
     def __init__(self):
-        self.raw_data_dir = "/data/tleek/undertale_raw_data/assemblage"
+        self.raw_data_dir = "~/undertale_shared/datasets/raw/assemblage"
+        logger.debug("In constructor")
 
-    def run(self, data, rank: int = 0, world_size: int = 1):
-        logger.warning("DANGER this dataset may contain malware so be careful with it")
-
-        bins_zip = "winpe_licensed.zip"
-        sql_zip = "winpe_licensed.sqlite.zip"
-
-        sqlfile = f"{self.raw_data_dir}/winpe.sqlite"
-        bins_dir = f"{self.raw_data_dir}/winpe"
-
-        if (not os.path.exists(sqlfile)) or (os.path.getsize(sqlfile) == 0):
-            os.system(
-                f"/usr/bin/unzip -o -q -d {self.raw_data_dir} {self.raw_data_dir}/{sql_zip}"
-            )
-        if not os.path.exists(bins_dir):
-            os.system(
-                f"/usr/bin/unzip -o -q -d {self.raw_data_dir} {self.raw_data_dir}/{bins_zip}"
-            )
-
-        logger.info("Unzipping complete")
-        tick()
+    def run(self, data, rank: int = 0, world_size: int = 1) -> DocumentsPipeline:
+        sqlfile = "../vcpkg.sqlite"
+        bins_dir = f"{self.raw_data_dir}/vcpkg"
 
         functions = {}
-        with sqlite.connect(sqlfile) as db:
+        with sqlite3.connect(sqlfile) as db:
+            logger.info("Connected to SQL database")
             cur = db.cursor()
 
             i = 0
             current_binary = None
             current_binary_id = None
-            num_with_source = 0
             num_failed_rvas = 0
 
             for row in cur.execute(
-                "SELECT f.id, b.id, b.file_name, b.repo_last_update, b.github_url, b.platform, b.build_mode, b.optimization, b.toolset_version, b.path, f.name, f.source_codes, r.start, r.end  FROM functions f INNER JOIN binaries b ON f.binary_id = b.id INNER JOIN rvas r ON f.id = r.function_id ORDER BY b.id;"
+                "SELECT f.id, b.id, b.file_name, b.repo_last_update, b.github_url, b.platform, b.build_mode, b.optimization, b.toolset_version, b.path, f.name, r.start, r.end  FROM functions f INNER JOIN binaries b ON f.binary_id = b.id INNER JOIN rvas r ON f.id = r.function_id ORDER BY b.id;"
             ):
                 (
                     f_id,
@@ -106,17 +83,13 @@ class AssemblageWindowsReader(BaseReader):
                     b_toolset_version,
                     b_path,
                     f_name,
-                    f_source,
                     r_start,
                     r_end,
                 ) = row
 
-                if not (b_platform == "x64"):
-                    continue
-
                 if i > 0 and (i % 100000) == 0:
                     logger.info(
-                        f"Progress... {i} functions so far, {float(num_with_source)/i:.4f} have source, {float(num_failed_rvas)/i:.7f} with bad rvas"
+                        f"Progress... {i} functions so far, {float(num_failed_rvas)/i:.7f} with bad rvas"
                     )
 
                 if i > 1000000:
@@ -128,7 +101,7 @@ class AssemblageWindowsReader(BaseReader):
 
                 try:
                     raw_data = current_binary.get_data(
-                        rva=r_start, length=r_end - r_start + 1
+                        rva=r_start, length=r_end - r_start
                     )
                 except:
                     logger.warn(
@@ -137,15 +110,9 @@ class AssemblageWindowsReader(BaseReader):
                     num_failed_rvas += 1
                     continue
 
-                if f_source == "":
-                    f_source = None
-                else:
-                    num_with_source += 1
-
                 tup = (
                     (r_start, r_end),
                     raw_data,
-                    f_source,
                     b_platform,
                     b_build_mode,
                     b_optimization,
@@ -159,9 +126,6 @@ class AssemblageWindowsReader(BaseReader):
                 )
                 if f_id not in functions:
                     functions[f_id] = []
-                # this could be just one of several binary chunks that
-                # correspond to this function so we accumlate them in
-                # a list here, associated with the function id
                 functions[f_id].append(tup)
                 i += 1
 
@@ -179,13 +143,11 @@ class AssemblageWindowsReader(BaseReader):
                 logger.info(f"{i} of {len(functions)} items processed")
                 tick()
 
-            all_source = ""
             arr.sort(key=lambda tup: tup[0][0])
             (min_a, max_a) = (0xFFFFFFFFFFFFFFFF, 0)
             for (
                 rng,
                 code,
-                source,
                 platform,
                 build_mode,
                 optimization,
@@ -199,13 +161,12 @@ class AssemblageWindowsReader(BaseReader):
             ) in arr:
                 min_a = min(rng[0], min_a)
                 max_a = max(rng[1], max_a)
-            all_code = bytearray(b"\x090" * (max_a - min_a + 1))
+            all_code = bytearray(b"\x90" * (max_a - min_a + 1))
             last_platform = None
-            all_source = ""
+
             for (
                 rng,
                 code,
-                source,
                 platform,
                 build_mode,
                 optimization,
@@ -224,8 +185,7 @@ class AssemblageWindowsReader(BaseReader):
                     assert compiler == last_compiler
                     assert fun_name == last_fun_name
                 all_code[rng[0] - min_a : rng[1] - min_a + 1] = code
-                if source is not None:
-                    all_source += source
+
                 (
                     _,
                     last_platform,
@@ -234,20 +194,14 @@ class AssemblageWindowsReader(BaseReader):
                     last_compiler,
                     last_fun_name,
                 ) = (rng, platform, build_mode, optimization, compiler, fun_name)
-            if all_source == "":
-                all_source = None
 
-            # if two functions have same value for this then they differ only by
-            # compiler and optimization, which means they are equivalent; this
-            # is used by the transform PairwiseContrastive
             equiv_class = (
                 f"{bin_github_url}-{bin_repo_last_update}-{bin_filename}-{fun_name}"
             )
 
-            # this is the binary code for a single function
             yield Document(
                 id=f"fid={f_id}",
-                text=all_code,  # b64encode(all_code).decode("utf-8"),
+                text=all_code,
                 metadata={
                     "binary": all_code,
                     "architecture": platform,
@@ -261,24 +215,79 @@ class AssemblageWindowsReader(BaseReader):
         tick()
 
 
-class AssemblageWindowsPublicDataset(Dataset):
-    name = "assemblage-windows-public-dataset"
+class AssemblageVcpkg(Dataset):
+    name = "assemblage-vcpkg-dt"
 
     def get_pipeline(self, input, writer, parallelism):
-        steps = [
-            AssemblageWindowsReader(),
-            RadareDisassembler(),
-        ]
-        steps.extend(writer)
 
-        # Note: fails here with `TypeError: cannot pickle '_thread.lock' object`
-        # import copy
-        # sc = copy.deepcopy(steps)
+        if input == "binaries":
+            executor = self.get_executor(0, input)
+            executor.pipeline.append(writer)
+            return executor
+        elif input == "r2":
+            executor = self.get_executor(0, input)
+            executor.depends.pipeline.append(
+                ParquetWriter(
+                    output_folder=f"{DEFAULT_DATASETS_DIRECTORY}assemblage-vcpkg-dt-disassembled",
+                    adapter=lambda self, doc: doc.metadata,
+                    max_file_size=100 * 1024 * 1024,
+                )
+            )
+            executor.pipeline.append(writer)
+            return executor
 
-        return self.get_executor(steps, tasks=parallelism)
+        return None
+
+    def get_executor(self, steps, input):
+        # Stage 0: Parse function bytes and metadata
+        slurm_parse = SlurmPipelineExecutor(
+            pipeline=[
+                create_directory,
+                AssemblageVcpkgReader(),
+            ],
+            venv_path=os.path.join(f"{Path.home()}/.conda/envs", "ut"),
+            logging_dir="~/undertale/logs",
+            time="12:00:00",
+            cpus_per_task=2,
+            mem_per_cpu_gb=4,
+            tasks=1,
+            job_name="parse_vcpkg",
+            partition="xeon-p8",
+            sbatch_args={"distribution": "cyclic:cyclic"},
+        )
+
+        # Stage 1: Disassemble binaries in parallel
+        slurm_disassemble = SlurmPipelineExecutor(
+            depends=slurm_parse,
+            pipeline=[
+                create_directory,
+                ParquetReader(
+                    data_folder=f"{DEFAULT_DATASETS_DIRECTORY}assemblage-vcpkg-dt",
+                    adapter=lambda self, data, path, id_in_file: {
+                        "id": id_in_file,
+                        "text": data["binary"],
+                        "metadata": data,
+                    },
+                ),
+                RadareDisassembler(),
+            ],
+            venv_path=os.path.join(f"{Path.home()}/.conda/envs", "ut"),
+            logging_dir="~/undertale/logs",
+            time="12:00:00",
+            cpus_per_task=2,
+            mem_per_cpu_gb=4,
+            tasks=64,
+            job_name="vcpkg_disassemble_r2",
+            partition="xeon-p8",
+            sbatch_args={"distribution": "cyclic:cyclic"},
+        )
+
+        if input == "binaries":
+            return slurm_parse
+        elif input == "r2":
+            return slurm_disassemble
+        return None
 
 
 if __name__ == "__main__":
-    # import pdb
-    # pdb.set_trace()
-    main(AssemblageWindowsPublicDataset)
+    main(AssemblageVcpkg)
