a74a6db91ea5706f3ebcefe8f0eb7010ee0c512c
Suhaas Narayan <40607921+Swashbuckler1@users.noreply.github.com>
2025-04-23 12:11:27 -0400

    feat: AssemblageVcpkg dataset complete (#35)
    Co-authored-by: Alexander Interrante-Grant <a.interrantegrant@gmail.com>
    Co-authored-by: Chris Connelly <connelly@ll.mit.edu>
    Co-authored-by: Tim Leek <trleek@gmail.com>

--- undertale/datasets/pipeline/disassemblers/radare2.py
+++ undertale/datasets/pipeline/disassemblers/radare2.py
@@ -1,41 +1,45 @@
-import json
-import logging
-
-import r2pipe
 from datatrove.data import DocumentsPipeline
 from datatrove.pipeline.base import PipelineStep
-
-logger = logging.getLogger(__name__)
 
 
 class RadareDisassembler(PipelineStep):
     type = "🔧 - DISASSEMBLER"
     name = "R - Radare"
 
+    _requires_dependencies = [
+        "r2pipe",
+    ]
+
     def __init__(self):
         super().__init__()
-
-    def disas_buf(self, buf):
-        self.r.cmd("s 0")
-        # resize the "file" radare is working on to fit the fn
-        self.r.cmd(f"r {len(buf)}")
-        # write the fn bytes into radare's "file"
-        self.r.cmd("wx " + (" ".join([f"{i:02x}" for i in buf])))
-        # light analysis which gets fns (presumably it also
-        # does more than just linear disassembly since it
-        # says it finds functions
-        self.r.cmd("aa")
-        # pdf is "print disassembly of function" j means json
-        pdf = self.r.cmd("pdfj")
-        try:
-            pdf_dict = json.loads(pdf)
-            return pdf_dict
-        except:
-            return {}
 
     def run(
         self, data: DocumentsPipeline, rank: int = 0, world_size: int = 1
     ) -> DocumentsPipeline:
+
+        import json
+
+        import r2pipe
+        from datatrove.utils.logging import logger
+
+        def disas_buf(buf):
+            self.r.cmd("s 0")
+            # resize the "file" radare is working on to fit the fn
+            self.r.cmd(f"r {len(buf)}")
+            # write the fn bytes into radare's "file"
+            self.r.cmd("wx " + (" ".join([f"{i:02x}" for i in buf])))
+            # light analysis which gets fns (presumably it also
+            # does more than just linear disassembly since it
+            # says it finds functions
+            self.r.cmd("aa")
+            # pdf is "print disassembly of function" j means json
+            pdf = self.r.cmd("pdfj")
+            try:
+                pdf_dict = json.loads(pdf)
+                return pdf_dict
+            except:
+                return {}
+
         if not data:
             return
 
@@ -57,7 +61,7 @@ class RadareDisassembler(PipelineStep):
                     num_too_big += 1
                     continue
 
-                d = self.disas_buf(code)
+                d = disas_buf(code)
 
                 disassembly = []
                 if "ops" in d.keys():
