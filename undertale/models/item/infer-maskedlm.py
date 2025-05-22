c554953ceda110af17562155e31d3d4dfd1eb3b5
ashokk01824 <ashoktk.in@gmail.com>
2025-05-22 15:01:29 -0400

    Added Cuda check

--- undertale/models/item/infer-maskedlm.py
+++ undertale/models/item/infer-maskedlm.py
@@ -17,19 +17,26 @@ if __name__ == "__main__":
     parser.add_argument("input", help="masked disassembly input to fill in")
 
     arguments = parser.parse_args()
-
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    if torch.cuda.is_available():
+        torch.cuda.set_device(0)
     tokenizer = item.tokenizer.load(arguments.tokenizer)
     config = item.InstructionTraceConfig.from_tokenizer(tokenizer)
     model = item.InstructionTraceEncoderTransformerForMaskedLM(config)
 
     model = model.from_pretrained(arguments.model, local_files_only=True)
+    model = model.to(device)
     model.eval()
 
     pretokens = item.tokenizer.pretokenize(arguments.input)
     encoded = tokenizer.encode(pretokens)
+    
     input_ids = torch.tensor((encoded.ids,), dtype=torch.long)
+    input_ids = input_ids.to(device)
+    
     attention_mask = torch.tensor((encoded.attention_mask,), dtype=torch.long)
-
+    attention_mask = attention_mask.to(device)
+    
     with torch.no_grad():
         output = model(input_ids, attention_mask)
 
