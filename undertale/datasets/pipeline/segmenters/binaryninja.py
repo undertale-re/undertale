70e71434b3008b7f6e883410c9d2b1325c217cc1
Suhaas Narayan <suhaas2002@gmail.com>
2025-06-11 17:25:10 -0400

    Saving

--- undertale/datasets/pipeline/segmenters/binaryninja.py
+++ undertale/datasets/pipeline/segmenters/binaryninja.py
@@ -22,7 +22,19 @@ class BinaryNinjaFunctionSegmenter(PipelineStep):
             SymbolType.ExternalSymbol,
             SymbolType.LibraryFunctionSymbol,
             SymbolType.ImportAddressSymbol, 
-            SymbolType.SymbolicFunctionSymbol]
+            SymbolType.SymbolicFunctionSymbol
+        ]
+        
+        SKIP_TOKENS = [
+                InstructionTextTokenType.AnnotationToken,
+                InstructionTextTokenType.StackVariableToken
+            ]
+
+        SYMBOL_TOKENS = [
+            InstructionTextTokenType.CodeSymbolToken,
+            InstructionTextTokenType.DataSymbolToken,
+            InstructionTextTokenType.ExternalSymbolToken
+        ]
 
         for document in data:
             code = document.text
@@ -34,6 +46,25 @@ class BinaryNinjaFunctionSegmenter(PipelineStep):
                     continue
                 
                 code = bv.read(fn.start, fn.total_bytes)
+
+                sorted_blocks = sorted(func.basic_blocks, key=lambda b: b.start)
+                
+                for block in sorted_blocks:
+                    disassembly = []
+                    for line in block.disassembly_text:
+                        idx, symbol_token = next(
+                            ((i, token) for i, token in enumerate(line.tokens) if token.type in symbol_tokens),
+                            (None, None)  # default if not found
+                        )
+                        if symbol_token:
+                            line.tokens[idx].text = f"0x{symbol_token.value:x}"
+                            # breakpoint()
+                        disasm_str = ''.join(token.text for token in line.tokens if token.type not in skip_tokens)
+                        disasm_str = ' '.join(disasm_str.strip().split())
+                        disassembly.append(disasm_str)
+                        # print(disasm_str)
+                    disassembly = "\n".join(disassembly)
+                    print(disassembly)
                 
                 metadata = document.metadata.copy()
 
