821bbb529a29bc02bbe5ed1ec03e36089633b588
Suhaas Narayan <suhaas2002@gmail.com>
2025-06-13 11:39:03 -0400

    Disassembly and cfg

--- undertale/datasets/pipeline/segmenters/binaryninja.py
+++ undertale/datasets/pipeline/segmenters/binaryninja.py
@@ -45,12 +45,13 @@ class BinaryNinjaFunctionSegmenter(PipelineStep):
                 if fn.is_thunk or fn.symbol.type in SKIP_TYPES:
                     continue
                 
+                disassembly = []
+                graph = nx.Graph()
+                nodes = {}
                 code = bv.read(fn.start, fn.total_bytes)
-
-                sorted_blocks = sorted(func.basic_blocks, key=lambda b: b.start)
                 
-                for block in sorted_blocks:
-                    disassembly = []
+                for block in fn.basic_blocks:
+                    block_disassembly = []
                     for line in block.disassembly_text:
                         idx, symbol_token = next(
                             ((i, token) for i, token in enumerate(line.tokens) if token.type in symbol_tokens),
@@ -58,13 +59,22 @@ class BinaryNinjaFunctionSegmenter(PipelineStep):
                         )
                         if symbol_token:
                             line.tokens[idx].text = f"0x{symbol_token.value:x}"
-                            # breakpoint()
                         disasm_str = ''.join(token.text for token in line.tokens if token.type not in skip_tokens)
                         disasm_str = ' '.join(disasm_str.strip().split())
                         disassembly.append(disasm_str)
-                        # print(disasm_str)
-                    disassembly = "\n".join(disassembly)
-                    print(disassembly)
+                    block_disassembly = "\n".join(block_disassembly)
+                    node = (block.start, block_disassembly)
+                    graph.add_node(node)
+                    nodes[block.start] = node
+                
+                for block in fn.basic_blocks:
+                    outgoing_edges = block.outgoing_edges
+                    dst_nodes = [str(edge)[str(edge).find('@')+1:-1] for edge in outgoing_edges]
+                    for i in dst_nodes:
+                        graph.add_edge(nodes[block.start], nodes[int(i, 16)])
+
+                for _, block in sorted(graph.nodes, key=lambda n: n[0]):
+                    disassembly.append(block)
                 
                 metadata = document.metadata.copy()
 
