1ade9aa39198a8b301b5a52d50f9142917132af9
Paul Gibby <paul.gibby@ll.mit.edu>
2025-07-03 14:01:43 -0400

    moving validation examples to pytorch lightning

--- undertale/models/item/pretrain-maskedlm.py
+++ undertale/models/item/pretrain-maskedlm.py
@@ -5,10 +5,11 @@ import os
 import torch
 import transformers
 from lightning import Trainer
-from lightning.pytorch.callbacks import ModelCheckpoint, TQDMProgressBar
+from lightning.pytorch.callbacks import Callback, ModelCheckpoint, TQDMProgressBar
 from lightning.pytorch.callbacks.early_stopping import EarlyStopping
 from lightning.pytorch.loggers import TensorBoardLogger
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, RandomSampler
+from torch.utils.tensorboard import SummaryWriter
 
 from ... import logging as undertale_logging
 from ...datasets.base import Dataset
@@ -23,6 +24,41 @@ class ProgressBar(TQDMProgressBar):
         items = super().get_metrics(trainer, model)
         items.pop("v_num", None)
         return items
+
+
+class ValidationCallback(Callback):
+    def __init__(self, dataloader):
+        super().__init__()
+        self.dataloader = dataloader
+
+    def on_validation_end(self, trainer, pl_module):
+        for batch in self.dataloader:
+            input_ids = batch.input_ids.to(pl_module.device)
+            attention_mask = batch.attention_mask.to(pl_module.device)
+            output = pl_module(input_ids, attention_mask)
+            filled = torch.where(
+                input_ids == pl_module.tok.token_to_id(tokenizer.TOKEN_MASK),
+                torch.argmax(output, dim=-1),
+                input_ids,
+            )
+            input_seq = (
+                pl_module.tok.decode(input_ids[0].tolist(), skip_special_tokens=False)
+                .replace("[NEXT]", "\n")
+                .replace("[PAD]", "")
+                .strip()
+            )
+            predicted = pl_module.tok.decode(
+                filled[0].tolist(), skip_special_tokens=False
+            )
+            predicted = (
+                predicted.replace(tokenizer.TOKEN_PAD, "")
+                .replace("[NEXT]", "\n")
+                .strip()
+            )
+            if isinstance(pl_module.logger.experiment, SummaryWriter):
+                pl_module.logger.experiment.add_text(
+                    "mask prediction", f"input: {input_seq}\n\noutput:{predicted}"
+                )
 
 
 if __name__ == "__main__":
@@ -59,6 +95,11 @@ if __name__ == "__main__":
         "-n", "--nodes", default=1, type=int, help="number of nodes to use"
     )
     parser.add_argument("-v", "--version", help="training run version name")
+    parser.add_argument(
+        "--validation",
+        action="store_true",
+        help="whether to output validation examples",
+    )
 
     arguments = parser.parse_args()
 
@@ -132,8 +173,21 @@ if __name__ == "__main__":
         version=arguments.version,
     )
 
+    if arguments.validation:
+        random_sampler = RandomSampler(dataset["test"], num_samples=5)
+        random_validation = DataLoader(
+            dataset["test"],
+            sampler=random_sampler,
+            batch_size=1,
+            collate_fn=collator,
+            num_workers=8,
+        )
+        validation_check = ValidationCallback(random_validation)
+        callbacks = [progress, checkpoint, stop, validation_check]
+    else:
+        callbacks = [progress, checkpoint, stop]
     trainer = Trainer(
-        callbacks=[progress, checkpoint, stop],
+        callbacks=callbacks,
         logger=logger,
         accelerator=arguments.accelerator,
         devices=arguments.devices,
