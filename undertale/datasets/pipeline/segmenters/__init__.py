456c03f23851f0f7a6b86f8db2321d887281afbb
tleek <trleek@gmail.com>
2025-08-13 10:22:53 -0400

    feat: segment and disassemble with rizin (#40)
    Co-authored-by: Suhaas Narayan <suhaas.narayan@ll.mit.edu>
    Co-authored-by: Alex Interrante-Grant <ainterr@ll.mit.edu>

--- undertale/datasets/pipeline/segmenters/__init__.py
+++ undertale/datasets/pipeline/segmenters/__init__.py
@@ -1,3 +1,4 @@
 from .binaryninja import BinaryNinjaFunctionSegmenter  # noqa: F401, F403
 from .ghidra import GhidraFunctionSegmenter  # noqa: F401, F403
 from .lief import LIEFFunctionSegmenter  # noqa: F401, F403
+from .rizin import RizinFunctionSegmentAndDisassemble  # noqa: F401, F403
