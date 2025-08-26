93e11263fcf60024df15e54198ee0aef94bae4af
Alex Interrante-Grant <a.interrantegrant@gmail.com>
2025-08-26 11:19:08 -0400

    update xlcost dataset

--- undertale/datasets/xlcost.py
+++ undertale/datasets/xlcost.py
@@ -1,15 +1,14 @@
 import hashlib
+import os
 
-from datatrove.pipeline.readers import HuggingFaceDatasetReader
+from datatrove.pipeline.readers import JsonlReader
 
 from .base import Dataset, main
 from .pipeline.compilers import CppCompiler
 from .pipeline.disassemblers import GhidraDisassembler
 
 
-def adapt_xlcost_from_huggingface(
-    self, data: dict, path: str, id_in_file: int | str
-) -> dict:
+def adapt_xlcost_from_raw(self, data: dict, path: str, id_in_file: int | str) -> dict:
 
     source = data["code"]
     identifier = hashlib.sha256(source.encode("utf-8")).hexdigest()
@@ -26,14 +25,13 @@ class XLCost(Dataset):
 
     def get_pipeline(self, input, writer, parallelism):
         def xlcost_dataset_reader_split_factory(language: str, split: str):
-            return HuggingFaceDatasetReader(
-                "codeparrot/xlcost-text-to-code",
-                {"name": f"{language}-program-level", "split": split},
-                adapter=adapt_xlcost_from_huggingface,
+            return JsonlReader(
+                os.path.join(input, f"{language}-program-level", f"{split}.json"),
+                adapter=adapt_xlcost_from_raw,
             )
 
         def xlcost_dataset_reader_language_factory(language: str):
-            for split in ("train", "test", "validation"):
+            for split in ("train", "test", "valid"):
                 yield xlcost_dataset_reader_split_factory(language, split)
 
         steps = [
