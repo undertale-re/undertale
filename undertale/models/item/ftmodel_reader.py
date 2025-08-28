3d8ce8f2f07741cb58ed18061e813ea2d3958290
Ashok T. Kumar <as28456@g52lambda01.llan.ll.mit.edu>
2025-08-28 10:22:39 -0400

    embedloss

--- undertale/models/item/ftmodel_reader.py
+++ undertale/models/item/ftmodel_reader.py
@@ -2,9 +2,25 @@ from .model import Defaults, TransformerEncoderForSequenceSimilarity,Transformer
 from lightning.pytorch.utilities.model_summary import ModelSummary
 from . import tokenizer
 import torch
+import argparse
+
 if __name__ == "__main__":
 
-    tok = tokenizer.load("item.tokenizer.json", sequence_length=512)
+    parser = argparse.ArgumentParser(
+        description="finetune the model on a pairwise embedding dataset",
+    )
+
+    parser.add_argument(
+        "-t", "--tokenizer", required=True, help="trained tokenizer file"
+    )
+
+    parser.add_argument(
+        "-f", "--checkpoint", required=True, help="checkpointmodel"
+    )
+
+    arguments = parser.parse_args()
+
+    tok = tokenizer.load(arguments.tokenizer, sequence_length=512)
     ''' 
     model = TransformerEncoderForSequenceSimilarity(
         depth=Defaults.depth,
@@ -15,21 +31,22 @@ if __name__ == "__main__":
         intermediate_dimensions=Defaults.intermediate_dimensions,
         dropout=Defaults.dropout,
         eps=Defaults.eps,
-        lr=Defaults.lr,
+        lr=Defaults.lr, 
         warmup=Defaults.warmup,
         #embedding_size=128, #ASK TODO REVISIT
         #embedding_dropout_prob=Defaults.dropout
     )
     '''
-    modelpath = "/panfs/g52-panfs/scratch/as28456/assemblage-windows-disassembled-finetune-embedding/version_1.0/checkpoints/epoch=6-train_loss=0.14-val_loss=0.20.ckpt"
+    modelpath = arguments.checkpoint #"/panfs/g52-panfs/scratch/as28456/assemblage-windows-disassembled-finetune-embedding/version_1.0/checkpoints/epoch=1-train_loss=0.19-val_loss=0.20.ckpt"
     model = TransformerEncoderForSequenceSimilarity.load_from_checkpoint(modelpath)
     #model.from_pretrained(modelpath, local_files_only=True)
     model_state_dict = model.state_dict() 
     #model = torch.load(modelpath) 
     #model_state_dict = model['state_dict']
-    for param_name, param_tensor in model_state_dict.items():
-        if (param_name =='encoder.embedding.token.weight'):
-            print(f"{param_name}\t{param_tensor}")
 
     summary = ModelSummary(model, max_depth=-1) # Use -1 to show all modules
     print(summary)
+    
+    for param_name, param_tensor in model_state_dict.items():
+        if (param_name =='encoder.embedding.token.weight' or param_name =='encoder.layers.11.ff.linear2'):
+            print(f"{param_name}\t{param_tensor}")
