f943d79d9e1f4b211484810ebf9c2d4aa454c971
Alex Interrante-Grant <a.interrantegrant@gmail.com>
2025-10-21 09:40:14 -0400

    refactor: Merge `OpenAISummarizer` and `VLLMSummarizer` (#62)
    Co-authored-by: Paul Gibby <paul.gibby@ll.mit.edu>

--- undertale/datasets/pipeline/summarizers/__init__.py
+++ undertale/datasets/pipeline/summarizers/__init__.py
@@ -1,4 +1,3 @@
-# from .openai import OpenAISummarizer  # noqa: F401, F403
-from .vllm_summaries import VLLMSummarizer
+from .vllm import VLLMSummarizer
 
-__all__ = [VLLMSummarizer]
+__all__ = ["VLLMSummarizer"]
