c6ad5b2c7737ce75facf435cb461c08ed03b610d
Alex Interrante-Grant <ainterr@ll.mit.edu>
2025-10-29 09:52:23 -0400

    added compile pipeline script for slurm

---
+++ undertale/datasets/scripts/compile.py
@@ -0,0 +1,50 @@
+"""Apply ITEM pretokenization to a given dataset."""
+
+import os
+from pathlib import Path
+
+from datatrove.pipeline.readers import ParquetReader
+
+from ..base import Dataset, main
+from ..pipeline.compilers import CppCompiler
+
+
+def adapt_dataset_from_parquet(
+    self, data: dict, path: str, id_in_file: int | str
+) -> dict:
+    return {
+        "id": data.pop("id", id_in_file),
+        "text": data.pop("code"),
+        "metadata": data,
+    }
+
+
+class Compiler(Dataset):
+    def get_pipeline(self, input, writer, parallelism):
+        """"""
+        steps = [
+            ParquetReader(
+                input,
+                adapter=adapt_dataset_from_parquet,
+            ),
+            CppCompiler(),
+        ]
+        steps.extend(writer)
+
+        return self.get_executor(
+            steps,
+            venv_path=os.path.join(f"{Path.home()}/.conda/envs", "undertale"),
+            time="48:00:00",
+            cpus_per_task=1,
+            mem_per_cpu_gb=8,
+            tasks=parallelism,
+            job_name="compile",
+            partition="xeon-p8",
+            sbatch_args={
+                "distribution": "cyclic:cyclic",
+            },
+        )
+
+
+if __name__ == "__main__":
+    main(Compiler)
