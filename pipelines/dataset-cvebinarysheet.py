286ebadb1259ec3f2367cc8171174e540d01da8d
Paul Gibby <paul.gibby@ll.mit.edu>
2026-01-21 10:21:17 -0500

    fix mypy errors

--- pipelines/dataset-cvebinarysheet.py
+++ pipelines/dataset-cvebinarysheet.py
@@ -6,13 +6,14 @@ import pandas as pd
 
 from undertale.logging import get_logger
 from undertale.parsers import DatasetPipelineArgumentParser
-from undertale.pipeline import Client, Cluster
-from undertale.utils import assert_path_exists
+from undertale.pipeline import Client, Cluster, fanout, flush
+from undertale.pipeline.binary import segment_and_disassemble_binary
+from undertale.utils import assert_path_exists, get_or_create_directory
 
 logger = get_logger(__name__)
 
 
-def parse_samples(input: str, output: str) -> str:
+def parse_samples(input: str, output: str, max_size: int = int(1e6)) -> list[str]:
     """Process cvebinarysheet into a parquet file
 
     Arguments:
@@ -24,29 +25,41 @@ def parse_samples(input: str, output: str) -> str:
     """
 
     input = assert_path_exists(input)
-    if not os.path.isdir(output):
-        os.mkdir(output)
+    output, created = get_or_create_directory(output)
+
+    if not created:
+        logger.info("loading dataset")
+        return [join(output, fname) for fname in os.listdir(output)]
 
     logger.info(f"parsing cvebinarysheet samples from {input!r} to {output!r}")
-
+    id = 0
+    outputs = []
     for project in os.listdir(input):
         documents = []
         for version in os.listdir(join(input, project)):
             if os.path.isdir(join(input, project, version)):
                 for floc in os.listdir(join(input, project, version)):
-                    if os.path.isfile(join(input, project, version, floc)):
-                        with open(join(input, project, version, floc), "rb") as f:
+                    file_path = join(input, project, version, floc)
+                    if (
+                        os.path.isfile(file_path)
+                        and os.path.getsize(file_path) < max_size
+                    ):
+                        with open(file_path, "rb") as f:
                             document: dict[str, Union[str, bytes]] = {
-                                "code": f.read(int(1e6)),
+                                "binary": f.read(max_size),
                                 "project": project,
                                 "version": version,
                                 "filename": floc,
+                                "id": str(id),
                             }
                         documents.append(document)
-        data = pd.DataFrame.from_dict(documents)
-        data.to_parquet(join(output, f"{project}.par"))
-
-    return output
+                        id += 1
+        if len(documents) > 0:
+            data = pd.DataFrame.from_dict(documents)
+            outputs.append(join(output, f"{project}.parquet"))
+            data.to_parquet(join(output, f"{project}.parquet"))
+        # data.to_parquet(join(output, "dataset.parquet"))
+    return outputs
 
 
 if __name__ == "__main__":
@@ -66,7 +79,27 @@ if __name__ == "__main__":
         parsed = client.submit(
             parse_samples, arguments.input, f"{arguments.output}-parsed"
         )
+        # parsed.result()
+        # chunks = client.submit(
+        #     resize_parquet,
+        #     parsed,
+        #     f"{arguments.output}-resized",
+        #     chunks=arguments.parallelism,
+        # )
+        # chunks.result()
+        # compiled = fanout(client, compile_cpp, chunks, f"{arguments.output}-compiled")
+        disassembled = fanout(
+            client,
+            segment_and_disassemble_binary,
+            parsed,
+            f"{arguments.output}-disassembled",
+        )
+        disassembled.result()
+        # merged = client.submit(
+        #     resize_parquet, disassembled, f"{arguments.output}", size="100MB"
+        # )
 
-        parsed.result()
+        # merged.result()
+        flush(client)
 
     logger.info("processing complete")
