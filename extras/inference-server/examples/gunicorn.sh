38d9983fdf285902e4402f93565486309a0daf86
Alex Interrante-Grant <a.interrantegrant@gmail.com>
2026-03-18 12:26:13 -0400

    add gunicorn dependency

--- extras/inference-server/examples/gunicorn.sh
+++ extras/inference-server/examples/gunicorn.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-exec gunicorn --workers=4 --bind 127.0.0.1:8000 inference.api:app
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+gunicorn --workers=4 --bind 127.0.0.1:5000 inference.api:app
