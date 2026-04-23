993f0f5b2d0510e03f518499623c8a545769c4d4
Alex Interrante-Grant <a.interrantegrant@gmail.com>
2026-04-23 11:42:15 -0400

    chore: increment version number post-release

--- undertale/__init__.py
+++ undertale/__init__.py
@@ -3,6 +3,6 @@ __author__ = "MIT Lincoln Laboratory"
 __description__ = "large language models (LLMs) for binary program understanding"
 __copyright__ = f"2025, {__author__}"
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 __doc__ = f"{__title__}: {__description__}."
