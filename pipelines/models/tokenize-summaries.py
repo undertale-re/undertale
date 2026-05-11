e42c413e4625847935a02c16aa9bf537742d2ceb
Alex Interrante-Grant <a.interrantegrant@gmail.com>
2026-05-11 14:51:55 -0400

    implement summary tokenization pipeline

---
+++ pipelines/models/tokenize-summaries.py
@@ -0,0 +1,55 @@
+from undertale.logging import get_logger
+from undertale.models.summarization import tokenize_summaries_gpt2
+from undertale.parsers import DatasetArgumentParser
+from undertale.pipeline import Client, Cluster, fanout, flush
+from undertale.pipeline.parquet import Repartition, modify_parquet
+
+logger = get_logger(__name__)
+
+
+if __name__ == "__main__":
+    parser = DatasetArgumentParser(description="summary tokenization")
+
+    parser.add_argument(
+        "-t",
+        "--tokenizer",
+        type=str,
+        required=True,
+        help="path to a trained GPT-2 tokenizer",
+    )
+
+    arguments = parser.parse_args()
+    parser.setup(arguments)
+
+    with (
+        Cluster(type=arguments.cluster, parallelism=arguments.parallelism) as cluster,
+        Client(cluster) as client,
+    ):
+        logger.info("tokenizing summaries")
+
+        chunks = client.submit(
+            modify_parquet,
+            arguments.input,
+            f"{arguments.output}-repartitioned",
+            [Repartition(chunks=arguments.parallelism)],
+        )
+        tokenized = fanout(
+            client,
+            tokenize_summaries_gpt2,
+            chunks,
+            f"{arguments.output}-processed",
+            tokenizer=arguments.tokenizer,
+        )
+
+        merged = client.submit(
+            modify_parquet,
+            tokenized,
+            arguments.output,
+            [Repartition(size="100MB")],
+        )
+
+        merged.result()
+
+        flush(client)
+
+    logger.info("tokenization complete")
