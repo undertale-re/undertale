b46d25c5d0b4e8f02417449514ffa97862f709e8
Alex Interrante-Grant <a.interrantegrant@gmail.com>
2026-05-13 09:55:26 -0400

    add HF cache argument to tokeize-summaries pipeline

--- pipelines/models/tokenize-summaries.py
+++ pipelines/models/tokenize-summaries.py
@@ -1,14 +1,23 @@
+import os
+
 from undertale.logging import get_logger
 from undertale.models.summarization import tokenize_summaries_gpt2
 from undertale.parsers import DatasetArgumentParser
 from undertale.pipeline import Client, Cluster, fanout, flush
 from undertale.pipeline.parquet import Repartition, modify_parquet
+from undertale.utils.models.cache.load import load as load_cache
 
 logger = get_logger(__name__)
 
 
 if __name__ == "__main__":
     parser = DatasetArgumentParser(description="summary tokenization")
+
+    parser.add_argument(
+        "-c",
+        "--cache",
+        help="path to a HuggingFace cache directory - if not provided, models will be downloaded as necessary",
+    )
 
     arguments = parser.parse_args()
     parser.setup(arguments)
@@ -17,6 +26,14 @@ if __name__ == "__main__":
         Cluster(type=arguments.cluster, parallelism=arguments.parallelism) as cluster,
         Client(cluster) as client,
     ):
+        if arguments.cache:
+            client.wait_for_workers(arguments.parallelism)
+
+            logger.info("loading HuggingFace cache")
+
+            client.run(load_cache, arguments.cache)
+            client.run(lambda: os.environ.update({"HF_HUB_OFFLINE": "1"}))
+
         logger.info("tokenizing summaries")
 
         chunks = client.submit(
