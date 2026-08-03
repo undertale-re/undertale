461b851f03a0d7e4f15cfcaa4f6515b56777ce87
Paul Gibby <paul.gibby@ll.mit.edu>
2026-08-03 16:28:56 -0400

    linting

--- extras/inference-server/inference/settings.py
+++ extras/inference-server/inference/settings.py
@@ -66,6 +66,7 @@ class MaskedLMCheckpoint(Setting):
     key = "maskedlm-checkpoint"
     default = "{workspace}/maskedlm.ckpt"
 
+
 class FunctionNamingCheckpoint(Setting):
     key = "function-naming-checkpoint"
     default = "{workspace}/fnaming.ckpt"
